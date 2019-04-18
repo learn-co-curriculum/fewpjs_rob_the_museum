bf069fcce39b17da13ac61015770e8792482be4d
Steven G. Harms <steven.harms@flatironschool.com>
2019-04-18 14:05:28 -0400

    Add tests

--- test/indexTest.js
+++ test/indexTest.js
@@ -1,23 +1,47 @@
-
-
-describe("index.html", () => {
-  it("includes 'hello'", () => {
-    expect(document.querySelector('body').innerHTML).to.include('hello')
-  })
-
-  it("includes div", () => {
-    expect(document.querySelector('body').innerHTML, "no empty div element found within body").to.include('<div></div>')
-  })
-
-
-})
-
+const chai = require('chai');
+const museumCode = require("../index.js")
 
 describe("index.js", () => {
-  it("exists", () => {
-    expect(testVar).to.exist
-    expect(testVar).to.deep.equal({}) //without .deep, {} does not equal {}
-    expect(testFunc).to.exist
-    expect(testFunc()).to.equal('hi')
+  describe("Defines critical classes", () => {
+
+    it("has a Gallery class", () => {
+      expect(museumCode.Gallery).to.exist
+    })
+
+    it("has a Work class", () => {
+      expect(museumCode.Work).to.exist
+    })
+
+    it("has a Artist class", () => {
+      expect(museumCode.Artist).to.exist
+    })
+
+    it("has a Museum class", () => {
+      expect(museumCode.Museum).to.exist
+    })
+  })
+
+  describe("The Museum Class", () => {
+    let met = new museumCode.Museum("The Metropolitan Museum of Art", "art_heist.csv")
+
+    it("finds the biggest gallery", () => {
+      expect(met.biggestGallery().name).to.eq("Red");
+    })
+
+    it("finds the smallest gallery", () => {
+      expect(met.smallestGallery().name).to.eq("Blue");
+    })
+
+    it("finds the most occurring artist", () => {
+      expect(met.artistMostOccurring().name).to.eq("Pablo Picasso");
+    })
+
+    it("finds the value of the works by Picasso", () => {
+      expect(met.valueOfArtist("Pablo Picasso")).to.eq(755300000);
+    })
+
+    it("finds the gallery with the biggest payday", () => {
+      expect(met.biggestPayday().name).to.eq("Red");
+    })
   })
 })
