9f836df8d435f142eff8156b053e0fd44f9e6c1b
Steven G. Harms <steven.harms@flatironschool.com>
2019-04-18 14:50:38 -0400

    Fix module import / export

--- index.js
+++ index.js
@@ -1,5 +1,13 @@
-const testVar = {}
+/* fs provides readfilSync which you need to read the file */
+const fs = require('fs')
+/* Your code after the following line */
+/* ================================================================================  */
 
-function testFunc() {
-  return "hi"
-}
+// Code away!
+
+
+/* ================================================================================  */
+/* Don't edit past here */
+/* You will need to define Artist, Museum, Work, Gallery above the line */
+
+module.exports = { Artist, Museum, Work, Gallery }
